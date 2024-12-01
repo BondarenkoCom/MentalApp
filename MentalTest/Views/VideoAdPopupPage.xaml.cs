8a394c004cd16a08b0cee403eab6d92e7f921fce
Bondarenko <Honor@ZOMBIE>
2024-12-01 17:18:55 +0700

    add accuracy for cards.

--- MentalTest/Views/VideoAdPopupPage.xaml.cs
+++ MentalTest/Views/VideoAdPopupPage.xaml.cs
@@ -3,36 +3,45 @@ using Rg.Plugins.Popup.Services;
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using Octane.Xamarin.Forms.VideoPlayer; // Correct namespace
 
 namespace MentalTest.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class VideoAdPopupPage : PopupPage
     {
+        private bool _videoWatched;
+
         public VideoAdPopupPage()
         {
             InitializeComponent();
             Console.WriteLine("VideoAdPopupPage initialized");
 
-            // Subscribe to the Completed event
             videoPlayer.Completed += VideoPlayer_Completed;
 
-            // Hide the "Close" button until the video ends
             closeButton.IsVisible = false;
+            _videoWatched = false;
         }
 
         private void VideoPlayer_Completed(object sender, EventArgs e)
         {
             Console.WriteLine("Video playback ended");
-            // Show the "Close" button when the video ends
             closeButton.IsVisible = true;
+
+            _videoWatched = true;
         }
 
         private async void OnCloseButtonClicked(object sender, EventArgs e)
         {
-            Console.WriteLine("Close button clicked");
-            await PopupNavigation.Instance.PopAsync();
+            if (_videoWatched)
+            {
+                Console.WriteLine("Close button clicked, video watched");
+                await PopupNavigation.Instance.PopAsync();
+            }
+            else
+            {
+                Console.WriteLine("Attempt to close before watching video");
+                await DisplayAlert("Watch the ad", "Please watch the full ad before closing.", "OK");
+            }
         }
     }
 }
