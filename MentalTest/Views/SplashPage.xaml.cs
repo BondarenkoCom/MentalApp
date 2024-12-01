2cf13165a23affce436d52e063ed4d00a2c9d561
Bondarenko <Honor@ZOMBIE>
2024-12-01 16:27:43 +0700

    new logo + loading animation

--- MentalTest/Views/SplashPage.xaml.cs
+++ MentalTest/Views/SplashPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,9 @@ namespace MentalTest.Views
         public SplashPage()
         {
             InitializeComponent();
+            StartLoadingAnimation();
         }
+
         protected override async void OnAppearing()
         {
             base.OnAppearing();
@@ -18,6 +21,23 @@ namespace MentalTest.Views
             await Task.Delay(2000);
 
             await Navigation.PushAsync(new NewTestSelectionPage());
+        }
+
+        private void StartLoadingAnimation()
+        {
+            Device.StartTimer(TimeSpan.FromMilliseconds(500), () =>
+            {
+                LoadingText.Opacity = LoadingText.Opacity == 1 ? 0.5 : 1;
+                return true; 
+            });
+
+            Device.StartTimer(TimeSpan.FromMilliseconds(300), () =>
+            {
+                Dot1.Opacity = (Dot1.Opacity + 0.3) % 1.2;
+                Dot2.Opacity = (Dot2.Opacity + 0.3) % 1.2;
+                Dot3.Opacity = (Dot3.Opacity + 0.3) % 1.2;
+                return true;
+            });
         }
     }
 }
