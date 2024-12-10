97d5a856ac26a1ee7847d7fa247cda1b4925c0f0
Bondarenko <Honor@ZOMBIE>
2024-12-10 19:40:48 +0700

    - heart animation - new fonts

--- MentalTest/AssemblyInfo.cs
+++ MentalTest/AssemblyInfo.cs
@@ -1,3 +1,6 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)] 
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Origram.otf", Alias = "OrigramFont")]
+
--- MentalTest/Views/SplashPage.xaml.cs
+++ MentalTest/Views/SplashPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -11,26 +12,63 @@ namespace MentalTest.Views
         public SplashPage()
         {
             InitializeComponent();
+
+            // Проверка доступности шрифта
+            CheckFontAvailability();
+
+            StartHeartBeatAnimation();
             StartLoadingAnimation();
+        }
+
+        private void CheckFontAvailability()
+        {
+            try
+            {
+                // Тестовая метка для проверки шрифта
+                var testLabel = new Label
+                {
+                    Text = "Font test: Origram",
+                    FontFamily = "Origram",
+                    FontSize = 18
+                };
+
+                Debug.WriteLine($"Font family set to: {testLabel.FontFamily}");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Font loading failed: {ex.Message}");
+            }
         }
 
         protected override async void OnAppearing()
         {
             base.OnAppearing();
 
-            await Task.Delay(3000);
+            await Task.Delay(7000);
 
             await Navigation.PushAsync(new NewTestSelectionPage());
         }
 
+        private void StartHeartBeatAnimation()
+        {
+            Device.StartTimer(TimeSpan.FromMilliseconds(1000), () =>
+            {
+                HeartLogo.ScaleTo(1.2, 300, Easing.CubicInOut)
+                        .ContinueWith(_ => HeartLogo.ScaleTo(1.0, 300, Easing.CubicInOut));
+                return true;
+            });
+        }
+
         private void StartLoadingAnimation()
         {
+            // Анимация текста
             Device.StartTimer(TimeSpan.FromMilliseconds(500), () =>
             {
                 LoadingText.Opacity = LoadingText.Opacity == 1 ? 0.5 : 1;
-                return true; 
+                return true;
             });
 
+            // Анимация точек
             Device.StartTimer(TimeSpan.FromMilliseconds(300), () =>
             {
                 Dot1.Opacity = (Dot1.Opacity + 0.3) % 1.2;
